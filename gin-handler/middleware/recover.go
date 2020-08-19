bd8949929e8a0b3c8105cb8376215a1aea7c11d0
SongLiangChen <462039091@qq.com>
2020-08-19 14:50:42 +0800

    1

--- gin-handler/middleware/recover.go
+++ gin-handler/middleware/recover.go
@@ -1,10 +1,11 @@
 package middleware
 
 import (
-	"github.com/SongLiangChen/common/gin-handler"
+	"runtime"
+
+	gin_handler "github.com/SongLiangChen/common/gin-handler"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-	"runtime"
 )
 
 // Recover the first gin'middleware to handle request,
@@ -12,13 +13,17 @@ import (
 // and panic exception will be logged to log file.
 //
 // defer() and recover() will be take about 20ns loss every request, but it still necessary.
-func Recover(c *gin.Context) {
+func Recover(c *gin.Context, pf ...func(*gin.Context)) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.WithFields(log.Fields{
 				"app":   c.GetString(gin_handler.KEY_APPNAME),
 				"stack": stack(),
 			}).Error(err)
+
+			for _, f := range pf {
+				f(c)
+			}
 		}
 	}()
 
