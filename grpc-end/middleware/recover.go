0ecf5e96870ad28f3550915de2bfc47dc39dd8a7
SongLiangChen <462039091@qq.com>
2020-08-19 15:19:10 +0800

    1

--- grpc-end/middleware/recover.go
+++ grpc-end/middleware/recover.go
@@ -1,9 +1,9 @@
 package middleware
 
 import (
-	"github.com/SongLiangChen/common/grpc-end"
 	"runtime"
 
+	grpc_end "github.com/SongLiangChen/common/grpc-end"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -28,4 +28,25 @@ func Recover(c *grpc_end.GRpcContext) {
 func stack() string {
 	var buf [2 << 10]byte
 	return string(buf[:runtime.Stack(buf[:], true)])
+}
+
+func RecoverWithCb(cb ...func(c *grpc_end.GRpcContext, stack string)) func(c *grpc_end.GRpcContext) {
+	return func(c *grpc_end.GRpcContext) {
+		defer func() {
+			if err := recover(); err != nil {
+				s := stack()
+
+				log.WithFields(log.Fields{
+					"app":   c.GetAppName(),
+					"stack": s,
+				}).Error(err)
+
+				for _, f := range cb {
+					f(c, s)
+				}
+			}
+		}()
+
+		c.Next()
+	}
 }
